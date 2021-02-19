ea37f0591027fe483bcafa3dc2dd6b32fdbb16e5
tundvgs <tu.nguyen@vgsholding.com>
2021-02-19 10:15:19 +0700

    first deploy

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log(111)
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+app.listen(3000,  () => console.log('app running port 3000'))
